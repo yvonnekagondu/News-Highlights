7614f7e86db963c2c07c6488b6d6a5735a72cbea
Yvonne1995 <ykagondu@gmail.com>
2018-04-08 13:05:35 +0300

    additionals

---
+++ app/main/errors.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from . import main
+
+@main app_errorhandler(404)
+def four_o_four(error):
+    """
+    Function to render 404 erroe page
+    """
+    return render_template('four_o_four.html'), 404
