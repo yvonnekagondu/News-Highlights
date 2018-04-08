e448beb57d6c2250af759b07388a20616512c098
Yvonne1995 <ykagondu@gmail.com>
2018-04-08 12:28:45 +0300

    additions again

--- app/request.py
+++ app/request.py
@@ -34,11 +34,11 @@ def get_sources(coutry, category):
 
     return source_results
 
-def process_results(news_list):
+def process_sources(source_list):
     """
     Function that processes new list dictionary and turns them to a list of objects
     Args:
-        news_list: A list of dictionaries that contain news sources_list
+        sources_list: A list of dictionaries that contain news sources_list
     Returns:
         news_results: A list of news objects
     """
@@ -63,9 +63,61 @@ def process_results(news_list):
     return news_results
 #The method get() returns a value for the given key. If key is not available then returns default value None.
 
+
+
 def get_articles(id):
     """
     Function that gets the json response to our url request
     """
     get_sources_news_url = source_url.format(
         id,api_key)
+    with urllib.request.urlopen(get_sources_news_url)as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_articles(news_results_list)
+
+    return news_results
+
+def process_articles(articles_list):
+    """
+    Function that processes new list dictionary and turns them to a list of objects
+    Args:
+        news_list: A list of dictionaries that contain news sources_list
+    Returns:
+        news_results: A list of news objects
+    """
+    news_results = []
+    source_dictionary = {}
+    for result in articles_list:
+        #We store the nested dictionary in source_id
+        source_id = result['source']
+        #We extrect and store it in our source dictionary
+        source_dictionary['id']= source_id['name']
+        id = source_dictionary['id']
+        name = source_dictionary['name']
+        print(name)
+        author = result.get('author')
+        title = result.get('description')
+        url = redult.get('url')
+        urltoImage = result.get('urltoImage')
+        publishedAt = result.get('publishedAt')
+
+    if urltoImage:
+        print(id)
+        source_object = Articles(id,
+                                name,
+                                author,
+                                title,
+                                description,
+                                url,
+                                urltoImage, publishedAt)
+        news_results.append(source_object)
+
+    return news_results
+
+        
