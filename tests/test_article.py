ce6473e61dae8da38c947270a0872827cdc2b632
Yvonne1995 <ykagondu@gmail.com>
2018-04-09 06:45:32 +0300

    tests and stuff

---
+++ tests/test_article.py
@@ -0,0 +1,21 @@
+import unittest
+from app.models import Source
+
+
+class NewsSource(unittest.TestCase):
+    """
+    Test Class to test the behaviour of the Source class
+    """
+    def setUp(self):
+        """
+        Set up method that runs before every TestCase
+        """
+        self.new_source = Source("ntv-news",
+                                "ntv-news",
+                                "north east west and south",
+                                "https://ntv.nation.co.ke",
+                                "general",
+                                "Kenya")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source, Source))
