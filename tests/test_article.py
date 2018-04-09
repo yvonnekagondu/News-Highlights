db5c2877b3835a5097025d84ecdfb294caa45e77
Yvonne1995 <ykagondu@gmail.com>
2018-04-09 07:01:30 +0300

    review of models

--- tests/test_article.py
+++ tests/test_article.py
@@ -1,21 +1,23 @@
 import unittest
-from app.models import Source
+from app.models import Articles
 
 
-class NewsSource(unittest.TestCase):
+class TestArticles(unittest.TestCase):
     """
-    Test Class to test the behaviour of the Source class
+    Test Class to test the behaviour of the Articles class
     """
     def setUp(self):
         """
         Set up method that runs before every TestCase
         """
-        self.new_source = Source("ntv-news",
+        self.new_articles = Articles("ntv-news",
                                 "ntv-news",
-                                "north east west and south",
-                                "https://ntv.nation.co.ke",
+                                "Nimro Taabu",
+                                "Mudavadi: I did not believe in Raila Odinga's 'swearing-in'",
+                                "National Super Alliance (NASA) co-principal Musalia Mudavadi has previously said that his skipping of Raila Odinga's 'swearing-in' did not make him a coward.",
+                                "https://www.standardmedia.co.ke/ktnnews/video/2000153075/-mudavadi-i-did-not-believe-in-raila-odinga-s-swearing-in",
                                 "general",
-                                "Kenya")
+                                "Sunday 8 Apr 2018 10:40 pm")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source, Source))
