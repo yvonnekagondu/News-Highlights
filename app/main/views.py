51f87b51b93656d6ea9b24f9b10599a2f42c75cd
Yvonne1995 <ykagondu@gmail.com>
2018-04-09 05:05:27 +0300

    additionals1

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,42 @@
-from flask
+from flask import render_template
+from . import main
+from ..request import get_sources, get_articles
+
+
+
+
+@main.route('/')
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
+
+    general_list = get_sources('us', 'general')
+    business_list = get_sources('us', 'business')
+    technoloogy_list = get_sources('us', 'technology')
+    sports_list = get_sources('us', 'sports')
+    health_list = get_sources('us', 'health')
+    science_list = get_sources('us', 'science')
+    entertainment_list = get_sources('us', 'entertainment')
+    test_args = 'Working!'
+    return render_template('index.html',
+                            test_param=test_args,
+                            general=general_list,
+                            business=business_list,
+                            technology=technoloogy_list,
+                            sports=sports_list,
+                            health=health_list,
+                            science=science_list
+                            entertainment=entertainment_list)
+
+
+@main.route('/news/<id>')
+def news(id):
+    """
+    View articles page function that returns the movie details page and its data
+    """
+    news_args = get_articles(id)
+    highlight_args = 'Route Working!!'
+    return render_template('news.html',
+                            highlight_param=highlight_args,
+                            news=news_args)
